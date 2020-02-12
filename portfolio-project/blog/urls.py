e1a28f2fde37c1affbc81488955645e744294406
yahyaelfandary <yahyaelfandary@gmail.com>
2020-02-12 09:45:33 +0200

    display modification

--- portfolio-project/blog/urls.py
+++ portfolio-project/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blogs,name='blogs'),
+    path('<int:blog_id>/', views.detail,name='detail'),
 ]
--- portfolio-project/blog/views.py
+++ portfolio-project/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import blog
+
 # Create your views here.
 def blogs(request):
     blogs = blog.objects
     return render(request,'blogs.html',{'blogs':blogs})
+def detail(request,blog_id):
+    detail_blog = get_object_or_404(blog,pk=blog_id)
+    return render(request,'detail.html',{'detail_blog':detail_blog})
