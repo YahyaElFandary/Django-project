0ed341935946093d24861ec4f69e565ce2781217
yahyaelfandary <yahyaelfandary@gmail.com>
2020-02-15 09:45:24 +0200

    pass

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Motherfucker1',
+        'PASSWORD':'markosblack',
         'HOST':'localhost',
         'PORT':'5432',
 
