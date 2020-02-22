a2f1644c3f99e14aaafdb77e4300ae3365c6d685
yahyaelfandary <yahyaelfandary@gmail.com>
2020-02-22 15:20:00 +0200

    login and logout

--- ProductHunt-project/accounts/views.py
+++ ProductHunt-project/accounts/views.py
@@ -32,7 +32,17 @@ def signup(request):
 
 
 def login(request):
-    return render(request,'login.html')
+    if request.method=='POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'login.html',{'error':'Username or Password are incorrect!'})
+    else:
+        return render(request,'login.html')
 
 def logout(request):
-    pass
+    if request.method=='POST':
+        auth.logout(request)
+        return redirect('home')
