06c63471c300e0ab88b56dbd8d13c717aeee1d21
yahyaelfandary <yahyaelfandary@gmail.com>
2020-02-23 13:16:17 +0200

    create project and upvote fun

--- ProductHunt-project/products/models.py
+++ ProductHunt-project/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(blank=False ,max_length=60)
+    url = models.TextField(blank=True)
+    pub_date = models.DateTimeField(blank=True, default=datetime.datetime.now)
+    votes_total = models.IntegerField(blank=True, default=1)
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")
+    body = models.TextField(blank=False)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
