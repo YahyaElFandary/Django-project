dde7c3b3cc4c8b6df7938fbc22599ba3fcfd6b7f
yahyaelfandary <yahyaelfandary@gmail.com>
2020-02-25 09:45:22 +0200

    FRONTEND IS READY

--- hotvidz-project/hotvidz/settings.py
+++ hotvidz-project/hotvidz/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
